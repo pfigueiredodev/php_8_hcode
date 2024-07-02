96ea985559440edf5c8e0c6e22494f0c18dbe3ea
pfigueiredodev1 <pfigueiredo@ciandt.com>
2024-07-02 17:15:06 -0300

    first commit

---
+++ comentarios.php
@@ -0,0 +1,19 @@
+<?php
+
+// Este é um comentário de linha
+
+// echo "Hcode";
+
+/*
+    functon somar() {
+        return 2 + 2;
+    }
+*/
+
+/**
+ * * Comentário destacado
+ * ! Comentário de alerta
+ * ? Dúvida
+ * TODO: A Fazer
+ * @param nome Informar o nome da pessoa
+ */
---
+++ tags-php.php
@@ -0,0 +1,3 @@
+<?php
+
+	echo "<h2>Bem-vindo ao curso de PHP 8</h2>";
